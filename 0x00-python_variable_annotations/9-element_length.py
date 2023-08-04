3a06dfd6a61c063b7c8d496bbbd2ad5b30ef7dc0
Jojo <jojothomas1515@gmail.com>
2023-08-04 19:52:31 +0100

    Refractoring

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module."""
+
+from typing import Sequence, Iterable, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """"""
+    return [(i, len(i)) for i in lst]
