fa35e7891c86b8e96ad7d1c9ad599239547989ca
Jojo <jojothomas1515@gmail.com>
2023-08-04 18:52:10 +0100

    tuple typing

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,4 +5,4 @@ from typing import Tuple, Union
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Take the arguments passed and makes it a tuple."""
-    return (k, float(v))
+    return (k, v)
