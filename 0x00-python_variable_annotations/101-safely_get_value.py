a1d0f58453a761259d67a41be102216b32d6bed1
Jojo <jojothomas1515@gmail.com>
2023-08-04 20:33:23 +0100

    Refractoring

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module."""
+
+from typing import TypeVar, Union, Any, Mapping
+
+T = TypeVar('~T')
+
+
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """Alot of annotation."""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
