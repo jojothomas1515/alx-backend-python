0a0a76f9ba83fee5528ebc1b427eb00feaace4e9
Jojo <jojothomas1515@gmail.com>
2023-08-04 19:11:27 +0100

    typing for function return a function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for function that return a multipiler function."""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Make multiplier function."""
+    return lambda x: x * make_multiplier
