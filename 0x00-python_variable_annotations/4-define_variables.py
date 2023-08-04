ef4074cd918c0564e3fed48896f5e702135c075a
Jojo <jojothomas1515@gmail.com>
2023-08-04 18:04:40 +0100

    variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Type annotation."""
-
 a: int = 1
 pi: int = 3.14
 i_understand_annotations: bool = True
--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,13 +1,13 @@
-from typing import Callable
+#!/usr/bin/env python3
 
+a = __import__('4-define_variables').a
+pi = __import__('4-define_variables').pi
+i_understand_annotations = __import__(
+    '4-define_variables').i_understand_annotations
+school = __import__('4-define_variables').school
 
-def return_function() -> Callable[[int], int]:
-
-    return lambda x, y: x + y
-
-
-jj: Callable[[int], int] = return_function()
-jj(2)
-
-
-all()
+print("a is a {} with a value of {}".format(type(a), a))
+print("pi is a {} with a value of {}".format(type(pi), pi))
+print("i_understand_annotations is a {} with a value of {}".format(
+    type(i_understand_annotations), i_understand_annotations))
+print("school is a {} with a value of {}".format(type(school), school))
