3afd9ef7cb42749d5976b64b6cbd64d2bd78dfd5
Jojo <jojothomas1515@gmail.com>
2023-08-04 18:14:50 +0100

    list annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Module for a function that sum a list."""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Sum a list."""
     acc: float = 0
     for i in input_list:
