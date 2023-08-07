82eb6ac7d671756b921fd19a8d81fed3e5d355ca
Jojo <jojothomas1515@gmail.com>
2023-08-07 20:15:45 +0100

    async

--- 0x01-python_async_function/1-main.py
+++ 0x01-python_async_function/1-main.py
@@ -7,5 +7,5 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 print(asyncio.run(wait_n(5, 5)))
-print(asyncio.run(wait_n(0, 7)))
-print(asyncio.run(wait_n(0, 0)))
+print(asyncio.run(wait_n(10, 7)))
+print(asyncio.run(wait_n(10, 0)))
