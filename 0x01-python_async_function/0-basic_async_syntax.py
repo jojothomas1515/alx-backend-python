b049c24536303120e67bae3d93444bd0fd404187
Jojo <jojothomas1515@gmail.com>
2023-08-07 20:46:17 +0100

    i dont know why this is not working

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,8 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Wait random  asynchronusly wait for a random amount of
+    Wait random  asynchronusly wait for a random amount of.
+
     time that is between the max_delay and 0 number of seconds.
     """
     secs = random.uniform(0, max_delay)
