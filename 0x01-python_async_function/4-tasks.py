88b753ea941968de3c7ee4ccf4de22cb97ccf6d7
Jojo <jojothomas1515@gmail.com>
2023-08-07 20:37:56 +0100

    async

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Module for functions that performs cuccurent operation."""
-from typing import List
 import asyncio
+from typing import List
+
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
