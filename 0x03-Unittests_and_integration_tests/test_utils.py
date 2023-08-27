43971daf92381c1afa183daf1c0437f622d48c69
Jojo <jojothomas1515@gmail.com>
2023-08-27 10:15:24 +0100

    test get json, mocking and patching the get method of requests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Test module for the util module."""
 
-from unittest import TestCase
-from utils import access_nested_map
+from unittest import TestCase, mock
+from utils import access_nested_map, get_json
 from parameterized import parameterized
+import fixtures
+import requests
 
 
 class TestAccessNestedMap(TestCase):
@@ -28,3 +30,26 @@ class TestAccessNestedMap(TestCase):
         """Test with parameterized expand."""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """
+    Test the get json function.
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, expected):
+        """Test get_json method behaviour.
+
+        Path the request get method to return a mocked object
+        with a json method.
+        """
+        mock_with_json = mock.MagicMock()
+        mock_with_json.json = lambda: expected
+        with mock.patch("requests.get", create=True,
+                        return_value=mock_with_json,) as mocked:
+            self.assertEqual(get_json(test_url), expected)
+            mocked.assert_called_once_with(test_url)
