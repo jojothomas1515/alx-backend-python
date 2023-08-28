336d17dc8c89b1807ade47e36c2d31d26a13681a
Jojo <jojothomas1515@gmail.com>
2023-08-28 18:02:54 +0100

    unittest for github org client, test public repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -38,3 +38,14 @@ class TestGithubOrgClient(unittest.TestCase):
                 "bsd-3-clause"), ['episodes.dart'])
             mk.assert_called_once()
             mocked.assert_called_once()
+
+    expand = [
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ]
+
+    @parameterized.expand(expand)
+    def test_has_license(self, repo, license_key, expected):
+        """Testing the has license static method."""
+        self.assertEqual(GithubOrgClient.has_license(
+            repo, license_key), expected)
